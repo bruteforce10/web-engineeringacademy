75393b9b0eb8746818c3f6f0dc4cc204ec2036d4
Firdi Audi Putra <39665978+bruteforce10@users.noreply.github.com>
2024-09-20 13:56:56 +0700

    fix reply page

--- app/reply/page.jsx
+++ app/reply/page.jsx
@@ -16,7 +16,7 @@ export default function ReplyPage() {
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch("http://localhost:3000/api/account", {
+      const response = await fetch("/api/account", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
