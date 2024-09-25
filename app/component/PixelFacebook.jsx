ed74a30b44300f30297f256db029fe7d7fa92333
Firdi Audi Putra <39665978+bruteforce10@users.noreply.github.com>
2024-09-25 15:39:41 +0700

    set pixels test 3

--- app/component/PixelFacebook.jsx
+++ app/component/PixelFacebook.jsx
@@ -1,22 +1,54 @@
 "use client";
 import React, { useEffect } from "react";
 import * as fbq from "@/lib/fpixel";
+import Script from "next/script";
 
 const PixelFacebook = ({ children }) => {
   useEffect(() => {
-    fbq.pageview(window.location.pathname);
+    if (!window.fbq) {
+      window.fbq = (...args) =>
+        window.fbq.callMethod
+          ? window.fbq.callMethod(...args)
+          : window.fbq.queue.push(...args);
+      window.fbq.push = window.fbq;
+      window.fbq.loaded = true;
+      window.fbq.version = "2.0";
+      window.fbq.queue = [];
+    }
 
-    const handleRouteChange = () => {
-      fbq.pageview(window.location.pathname);
-    };
-
-    window.addEventListener("popstate", handleRouteChange);
-    return () => {
-      window.removeEventListener("popstate", handleRouteChange);
-    };
+    window.fbq("init", fbq.FB_PIXEL_ID);
+    window.fbq("track", "PageView");
   }, []);
-
-  return <>{children}</>;
+  return (
+    <>
+      <Script
+        id="facebook-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', ${fbq.FB_PIXEL_ID});
+          `,
+        }}
+      />
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src={`https://www.facebook.com/tr?id=${fbq.FB_PIXEL_ID}&ev=PageView&noscript=1`}
+        />
+      </noscript>
+      {children}
+    </>
+  );
 };
 
 export default PixelFacebook;
--- app/layout.jsx
+++ app/layout.jsx
@@ -4,9 +4,8 @@ import Navbar from "./component/Navbar";
 import { AppContextProvider } from "@/utils/context/AppContext";
 import Footer from "./component/Footer";
 import CallToAction from "./component/CallToAction";
-import Script from "next/script";
-import * as fbq from "../lib/fpixel";
 import PixelFacebook from "./component/PixelFacebook";
+import dynamic from "next/dynamic";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -20,29 +19,14 @@ export const metadata = {
     "Dapatkan akses seumur hidup ke lebih dari 106GB file teknik sipil & arsitektur. Mulai dari gambar 2D/3D, tutorial software, ebook, modul, hingga format Excel teknik sipil. Engineering Wakanda menyediakan semua yang Anda butuhkan untuk proyek, pembelajaran, dan referensi teknik sipil. Bonus lengkap termasuk SOP kontraktor dan software pendukung. Investasi terbaik untuk pengetahuan masa depan Anda!",
 };
 
+const NoSSRPixelFacebook = dynamic(() => import("./component/PixelFacebook"), {
+  ssr: false,
+});
+
 export default function RootLayout({ children }) {
   return (
-    <PixelFacebook>
-      <html lang="en">
-        <head>
-          <Script
-            id="fb-pixel"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', ${fbq.FB_PIXEL_ID});
-            `,
-            }}
-          />
-        </head>
+    <html lang="en">
+      <NoSSRPixelFacebook>
         <AppContextProvider>
           <body className={inter.variable + "bg-[#F6F8FD] "}>
             <Navbar />
@@ -53,7 +37,7 @@ export default function RootLayout({ children }) {
             </div>
           </body>
         </AppContextProvider>
-      </html>
-    </PixelFacebook>
+      </NoSSRPixelFacebook>
+    </html>
   );
 }
